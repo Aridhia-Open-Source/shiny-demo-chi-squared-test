faca4beb08693e1dda8da4c961bd177c945d9e97
James Bonner <james.bonner@aridhia.com>
2017-06-22 14:46:25 +0100

    update documentation

--- workspace/mini-apps/chi_squared_test/documentation_ui.R
+++ workspace/mini-apps/chi_squared_test/documentation_ui.R
@@ -4,16 +4,16 @@ documentation_tab <- function() {
                      fluidRow(column(
                        6,
                        h3("Chi-squared tests: goodness-of-fit and independence of nominal variables"), # paragraph and bold text
-                       p("This app allows you to perform a chi-squared test on nominal variables and test the degree of independence between different nominal
+                       p("This mini-app allows you to perform a chi-squared test on nominal variables and test the degree of independence between different nominal
              variables. The data containing these variables can be selected from your workspace, or manually input in a raw or tabulated format."),
                        
-                       h4("App layout"),
-                       p("The app contains three tabs; this tab gives you an overview of the app itself. The Test of Goodness of Fit and Test of Independence 
+                       h4("Mini-app layout"),
+                       p("The mini-app contains three tabs; this tab gives you an overview of the mini-app itself. The Test of Goodness of Fit and Test of Independence 
              tabs contain a left-hand data selection sidebar and the output, displayed as both plots and statistical results."),              
                        
-                       h4("To use the app"),
+                       h4("To use the mini-app"),
                        p("To experiment with either test, first click on the relevant tab. From there, in the left-hand sidebar, you may click to select data
-              either from the workspace, raw or tabulated manual input. If selecting data from the workspace, the Refresh Table List button can be
+              either from the workspace, raw or tabulated manual input. If selecting data from the workspace, the Refresh table list button can be
               used to update the list if datasets are changed."),
                        p("When selecting variables, be sure to select NOMINAL variables only. If numeric variable such as age are selected, each category will 
               have a count of 1, and outputs will not display correctly."),
@@ -26,7 +26,7 @@ documentation_tab <- function() {
                      select the values of interest which appear below the drop-down menus for each.")
                        ),
                        
-                       p("The results of each test display as visual plots next to the sidebar, with statistical outputs shown further down the page.")
+                       p("The results of each test display as plots next to the sidebar, with statistical outputs shown further down the page.")
                      ),
                      column(
                        6,
